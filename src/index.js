ab9e961cf708de45b43736d2fb4e51906b39ac22
Valentin Lopan <valiantsin_lopan@live.com>
2018-09-15 14:21:17 +0300

    inplement getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,37 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let triangles = 0;
+    var ignoredIndexes = [];
+    for ( var i = 0; i < preferences.length; i++ ){
+        let first;
+        let second;
+        let third;
+        if(ignoredIndexes.includes(i)){
+            continue;
+        }
+        first = preferences[i];
+        if( ignoredIndexes.includes(first - 1)){
+            continue;
+        }
+        else{
+            second = preferences[first - 1];
+        }
+        if( ignoredIndexes.includes(second - 1)){
+            continue;
+        }
+        else{
+            third = preferences[second - 1];
+        }
+        if(i == 250){
+            console.log("lol");
+        }
+        if( (third == i + 1) && (first !== second) && (second!== first) ){
+            triangles++;
+            ignoredIndexes.push(i);
+            ignoredIndexes.push(first - 1);
+            ignoredIndexes.push(second - 1);
+        }
+    }
+    return triangles;
 };
